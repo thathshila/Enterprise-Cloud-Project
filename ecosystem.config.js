e4fc0bf8ea690c163463141c5b52164a2c3f0129
Thathshila <thathshilaashanganie9501@gmail.com>
2026-03-31 00:06:48 +0530

    ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "config-server",
+      script: "java",
+      args: "-jar config-server/target/config-server-0.0.1-SNAPSHOT.jar",
+      log-file: "./logs/config-server.log",
+    },
+    {
+      name: "service-registry",
+      script: "java",
+      args: "-jar service-registry/target/service-registry-0.0.1-SNAPSHOT.jar",
+      log-file: "./logs/service-registry.log",
+    },
+    {
+      name: "api-gateway",
+      script: "java",
+      args: "-jar api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
+      log-file: "./logs/api-gateway.log",
+    }
+  ]
+};
